c6148c86c0536d98227a3088df4366c485f5b527
Rodrigo Vitor <rodrigovitor@maxtool.com.br>
2020-06-03 22:42:29 -0300

    configuracoes do webpack

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-// Object shor syntax
+import { soma }  from './funcoes';
 
-const nome = 'Diego';
-const idade = 23;
-
-const usuario = {
-  nome,
-  idade,
-  cidade: 'Rio do Sul'
-};
-
-console.log(usuario);
+console.log(soma(1,4));
